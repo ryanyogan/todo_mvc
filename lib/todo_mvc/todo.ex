3c336c101b7d9d83759efbf6292f6c9a9b51ead6
ryanyogan <ryanyogan@gmail.com>
2020-01-20 16:09:08 -0800

    Toggle a todo state to active/completed

--- lib/todo_mvc/todo.ex
+++ lib/todo_mvc/todo.ex
@@ -1,8 +1,15 @@
 defmodule TodoMVC.Todo do
+  @todo __MODULE__
   @enforce_keys [:id, :text, :state]
   defstruct [:id, :text, :state, :editing]
 
   def new(text) do
-    %__MODULE__{id: UUID.uuid4(), text: text, state: "active"}
+    %@todo{id: UUID.uuid4(), text: text, state: "active"}
   end
+
+  def toggle(%@todo{state: "active"} = todo), do: complete(todo)
+  def toggle(%@todo{state: "completed"} = todo), do: activate(todo)
+
+  def complete(todo), do: %{todo | state: "completed"}
+  def activate(todo), do: %{todo | state: "active"}
 end
--- lib/todo_mvc_web/views/main_view.ex
+++ lib/todo_mvc_web/views/main_view.ex
@@ -1,3 +1,7 @@
 defmodule TodoMVCWeb.MainView do
   use TodoMVCWeb, :view
+  alias TodoMVC.Todo
+
+  def todo_classes(%Todo{state: "completed"}), do: "completed"
+  def todo_classes(_), do: ""
 end
